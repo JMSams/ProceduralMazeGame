4111679e591369746410c7975444d596ed04f06a
Jamie M Sams <j.sams@uos.ac.uk>
2017-11-10 19:40:14 +0000

    Initial commit.

---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FallingSloth.ProceduralMazeGame
+{
+    public class Maze : MonoBehaviour
+    {
+        Tile[,] cells;
+        Vector2 startPos;
+        Vector2 endPos;
+        List<Vector2> deadEnds;
+
+        public Vector2 gridSize;
+        public Tile tilePrefab;
+
+        public List<Sprite> tileSprites;
+
+        void Start()
+        {
+            GenerateMaze();
+        }
+
+        void GenerateMaze()
+        {
+            cells = new Tile[(int)gridSize.x, (int)gridSize.y];
+            deadEnds = new List<Vector2>();
+            startPos = new Vector2(Random.Range(0, gridSize.x - 1), Random.Range(0, gridSize.y - 1));
+
+            RecursiveBacktracker((int)startPos.x, (int)startPos.y);
+
+            if (deadEnds.Count == 0)
+                throw new System.Exception("Somehow, there are no dead ends!?");
+            else
+            {
+                while (true)
+                {
+                    int i = Random.Range(0, deadEnds.Count);
+                    if (cells[(int)deadEnds[i].x, (int)deadEnds[i].y].isStart)
+                    {
+                        endPos = deadEnds[Random.Range(0, deadEnds.Count)];
+                        cells[(int)endPos.x, (int)endPos.y].isEnd = true;
+                        break;
+                    }
+                }
+            }
+
+            cells[(int)endPos.x, (int)endPos.y] |= CellStates.IsEnd;
+        }
+
+        IEnumerator RecursiveBacktracker(int x, int y)
+        {
+            // To make a recursive call:
+            // yield return StartCoroutine(RecursiveBacktracker(x, y));
+        }
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace FallingSloth.ProceduralMazeGame
+{
+    public class Tile : MonoBehaviour
+    {
+        new SpriteRenderer renderer;
+
+        [HideInInspector]
+        public bool northCorridor, eastCorridor, southCorridor, westCorridor, isStart, isEnd;
+
+        void Awake()
+        {
+            renderer = GetComponent<SpriteRenderer>();
+        }
+    }
+}
